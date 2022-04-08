ae551addfdd3e086ded0d6c509dfa45168ef5d42
Thethickinnchickin <matt.reiley@sjsu.edu>
2022-04-07 22:35:21 -0700

    Deploy Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,10 +50,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308  
-    target : process.env.HOST,
+    target : URL,
     proxy: true,
     baseURL: URL,
   },
+
+
 
   target: 'server',
 
